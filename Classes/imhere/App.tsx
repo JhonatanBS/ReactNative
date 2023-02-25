ecc41c06927f2b325f52a0dea04cc23f788633fd
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-02-24 23:06:52 -0300

    Understading the concept of JSX

--- Classes/imhere/App.tsx
+++ Classes/imhere/App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(
+    <>
+      <Text>React</Text>
+      <Text>Native</Text>
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

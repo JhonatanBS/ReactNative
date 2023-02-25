48048c79bdbb650b220dbe4f8532330af003ebce
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-02-25 16:05:21 -0300

    Creating styles using the property StyleSheet

--- Classes/imhere/App.tsx
+++ Classes/imhere/App.tsx
@@ -1,30 +1,35 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return(
-    <View style={{
-      flex: 1,
-      backgroundColor: "#212121",
-      padding: 24,
-    }}>
-      <Text style={{
-        color: "#FFFFFF",
-        fontSize: 50,
-        fontWeight: "bold",
-        textAlign: "center"
-      }}>
-        Hello
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Learning React-Native
       </Text>
 
-      <Text style={{
-        color: "#FFFFFF",
-        fontSize: 50,
-        fontWeight: "bold",
-        textAlign: "center"
-      }}>
-        World!
+      <Text style={styles.date}>
+        November 25th, 2023
       </Text>
 
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#212121",
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  date: {
+    color: "#FDFCFE",
+    fontSize: 14,
+    fontWeight: "bold",
+  }
+
+});

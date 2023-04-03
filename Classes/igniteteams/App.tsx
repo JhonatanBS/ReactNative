bcb3c7115b392210bb0f92ef1169322be6b5e7dd
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-04-03 18:03:53 -0300

    Defining application's theme

--- Classes/igniteteams/App.tsx
+++ Classes/igniteteams/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from "styled-components";
+
+import theme from "./src/theme";
+
 import { Groups } from "@screens/Groups";
 
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
 

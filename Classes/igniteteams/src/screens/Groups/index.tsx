f43968e6c6ef8d06575171afd25d1f87b670b426
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-04-15 20:39:58 -0300

    Listing of Groups

--- Classes/igniteteams/src/screens/Groups/index.tsx
+++ Classes/igniteteams/src/screens/Groups/index.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
@@ -5,6 +9,8 @@ import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 
 export function Groups() {
+  const [ groups, setGroups] = useState<string[]>(["Team on the ignite"]);
+
   return (
     <Container >
       <Header />
@@ -13,7 +19,16 @@ export function Groups() {
         subtitle="Play with your Team"
       />
 
-      <GroupCard title="Team on the ignite" />
+      <FlatList 
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({item}) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
+      />
+
     </Container>
   );
 }

7623cb760b511815dfde80e18ed3622110636bd4
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-04-15 22:03:09 -0300

    Creating component Button

--- Classes/igniteteams/src/screens/Groups/index.tsx
+++ Classes/igniteteams/src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
@@ -33,6 +34,9 @@ export function Groups() {
         )}
       />
 
+    <Button 
+      title="Create a new team"
+    />
     </Container>
   );
 }

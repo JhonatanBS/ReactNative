9871c1bf43e35f29b76546964b2d6fb7e9726d1f
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-04-17 22:56:46 -0300

    Leaving icon dynamic

--- Classes/igniteteams/src/screens/Players/index.tsx
+++ Classes/igniteteams/src/screens/Players/index.tsx
@@ -1,16 +1,34 @@
-import { Container } from "./styles";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Container, Form } from "./styles";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
 export function Players() {
-  return(
+  return (
     <Container>
-      <Header showBackButton/>
-      <Highlight 
+
+      <Header showBackButton />
+
+      <Highlight
         title="Name of Team"
         subtitle="Add your player and separate the teams"
       />
+
+      <Form>
+
+        <Input
+          placeholder="Name of the person"
+          autoCorrect={false}
+        />
+
+        <ButtonIcon
+          icon="add"
+        />
+
+      </Form>
+
     </Container>
   )
 }

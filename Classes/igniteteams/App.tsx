46212b61e4ebbde4ba6df6f88186d18062db4a6d
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-04-20 22:28:01 -0300

    Structure of Navigation

--- Classes/igniteteams/App.tsx
+++ Classes/igniteteams/App.tsx
@@ -6,7 +6,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/
 
 import theme from "./src/theme";
 
-import { Groups } from "@screens/Groups";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </ThemeProvider>
   );
 }

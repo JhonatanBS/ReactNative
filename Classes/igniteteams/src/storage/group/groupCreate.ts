a4dd50a5588a2d1f40cad0e5d0e512b5b7257ec6
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-04-22 22:50:35 -0300

    Creating a class of error AppError

--- Classes/igniteteams/src/storage/group/groupCreate.ts
+++ Classes/igniteteams/src/storage/group/groupCreate.ts
@@ -2,11 +2,18 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { groupsGetAll } from "./groupsGetAll";
 import { GROUP_COLLECTION } from "@storage/storageConfig";
+import { AppError } from "@utils/AppError";
 
 export async function groupCreate(newGroup: string) {
   try {
 
     const storedGroups = await groupsGetAll();
+
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+
+    if(groupAlreadyExists) {
+      throw new AppError("Group Already exists!");
+    }
 
     const storage = JSON.stringify([...storedGroups, newGroup]);
 

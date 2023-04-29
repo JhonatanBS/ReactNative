dfef6a89bab40ba4ca7387e2bbb4e196e1dd59f2
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-04-28 22:38:55 -0300

    Customizing status Bar

--- Classes/ignitegym/App.tsx
+++ Classes/ignitegym/App.tsx
@@ -1,12 +1,18 @@
 
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#202024"}}>
+      <StatusBar 
+        barStyle={'light-content'}
+        backgroundColor="transparent"
+        translucent
+      />
+      
       {fontsLoaded ? <Text>Hello World Ignite Gym</Text> : <View />}
     </View>
   );

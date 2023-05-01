5a92480c6a78b5503c4eed05a5fc35c6ab7a017d
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-05-01 09:14:45 -0300

    Inserting and utilizing SVG with type and componentization

--- Classes/ignitegym/src/screens/SignIn.tsx
+++ Classes/ignitegym/src/screens/SignIn.tsx
@@ -1,6 +1,7 @@
-import { VStack , Image } from "native-base";
+import { VStack , Image, Text, Center } from "native-base";
 
 import BackgroundImg from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg";
 
 export function SignIn() {
   return(
@@ -11,6 +12,14 @@ export function SignIn() {
         resizeMode="contain"
         position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o corpo
+        </Text>
+      </Center>
     </VStack>
   )
 }

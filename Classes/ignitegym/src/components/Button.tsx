a376f9e027a022ba88669060a6f22a148ff64783
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-05-01 11:02:47 -0300

    Creating component Button with Native Base

---
+++ Classes/ignitegym/src/components/Button.tsx
@@ -0,0 +1,26 @@
+import { Button as NativeBaseButton, IButtonProps, Text } from "native-base";
+
+type Props = IButtonProps & {
+  title: string;
+}
+
+export function Button({ title, ...rest }: Props) {
+  return(
+    <NativeBaseButton 
+      w="full"
+      h={14}
+      bg="green.700"
+      rounded="sm"
+      _pressed={{bg: "green.500"}}
+      {...rest}
+    >
+      <Text 
+        color="white"
+        fontFamily="heading"
+        fontSize="sm"
+      >
+        { title }
+      </Text>
+    </NativeBaseButton>
+  )
+}

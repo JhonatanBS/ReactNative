e84c8440feb83611b0217cc83c4796948b11a5fb
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-04-30 22:16:20 -0300

    Creating and applying theme

--- Classes/ignitegym/App.tsx
+++ Classes/ignitegym/App.tsx
@@ -1,19 +1,23 @@
 import { NativeBaseProvider } from "native-base";
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+
+import { Loading } from "@components/Loading";
+
+import { THEME } from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
 
   return (
-    <NativeBaseProvider >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle={'light-content'}
         backgroundColor="transparent"
         translucent
       />
       
-      {fontsLoaded ? <View /> : <View />}
+      {fontsLoaded ? <View /> : < Loading />}
     </NativeBaseProvider>
   );
 }

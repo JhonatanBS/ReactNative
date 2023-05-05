c0f44e14f4dafa630670e864f131fb28d782f973
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-05-05 09:20:32 -0300

    Created screens: Home, History, Profile, Exercise

---
+++ Classes/ignitegym/src/screens/Exercise.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export function Exercise() {
+  return (
+    <Center flex={1}>
+      <Text>
+      Exercise
+      </Text>
+    </Center>
+  )
+}
---
+++ Classes/ignitegym/src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export function Home() {
+  return (
+    <Center flex={1}>
+      <Text>
+        Home
+      </Text>
+    </Center>
+  )
+}

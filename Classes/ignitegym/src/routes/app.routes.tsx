7d12b0b40156a513a6764dc3c217517366c33281
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-05-05 09:49:11 -0300

    Removing Header and label of menu

--- Classes/ignitegym/src/routes/app.routes.tsx
+++ Classes/ignitegym/src/routes/app.routes.tsx
@@ -1,15 +1,27 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator , BottomTabNavigationProp} from "@react-navigation/bottom-tabs";
 
 import { Home } from "@screens/Home";
 import { History } from "@screens/History";
 import { Profile } from "@screens/Profile";
 import { Exercise } from "@screens/Exercise";
 
-const { Navigator, Screen } = createBottomTabNavigator();
+type AppRoutes = {
+  home: undefined;
+  exercise: undefined;
+  profile: undefined;
+  history: undefined;
+}
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{
+      headerShown: false,
+      tabBarShowLabel: false
+    }}>
       <Screen
         name="home"
         component={Home}

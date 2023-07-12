ed10764e1482ba16b74fb4a9daf789a46d0411f9
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2023-07-12 14:56:11 -0300

    Show button selected

--- Classes/ignitegym/src/screens/Home.tsx
+++ Classes/ignitegym/src/screens/Home.tsx
@@ -1,16 +1,29 @@
-import { Center, Text, VStack } from "native-base";
+import { VStack, HStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
+import { useState } from "react";
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState("costa");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group 
-        name="costas"
-      />
+      <HStack>
+        <Group 
+        name="costas" 
+        isActive={groupSelected === "costa"} 
+        onPress={() => setGroupSelected("costa")}
+        />
+
+        <Group 
+        name="ombro" 
+        isActive={groupSelected === "ombro"} 
+        onPress={() => setGroupSelected("ombro")}
+        />
+      </HStack>
     </VStack>
   )
 }

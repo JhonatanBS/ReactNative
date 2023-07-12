66b2beab7a8d0cbf97d11f2e01593e2fea1489d9
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2023-07-12 16:10:52 -0300

    Creating component ExerciseCard

--- Classes/ignitegym/src/screens/Home.tsx
+++ Classes/ignitegym/src/screens/Home.tsx
@@ -3,6 +3,7 @@ import { VStack, FlatList, HStack, Heading, Text } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
+import { ExerciseCard } from "@components/ExerciseCard";
 
 export function Home() {
   const [groups, setGroups] = useState(["Costas", "Tríceps", "Bíceps", "ombro"]);
@@ -39,6 +40,9 @@ export function Home() {
           4
         </Text>
       </HStack>
+
+      <ExerciseCard />
+      <ExerciseCard />
       </VStack>
     </VStack>
   )

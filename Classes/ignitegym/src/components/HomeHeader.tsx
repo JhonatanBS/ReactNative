74fe49cb8dc21e5db62c7678c12a964b2e35ca7a
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2023-07-12 09:33:03 -0300

    Component UserPhoto

--- Classes/ignitegym/src/components/HomeHeader.tsx
+++ Classes/ignitegym/src/components/HomeHeader.tsx
@@ -1,8 +1,15 @@
 import { HStack, Heading, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto 
+        source={{ uri: "https://github.com/rodrigorgtic.png"}}
+        alt="Image of Profile"
+        size={16}
+        mr={4}
+      />
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá,

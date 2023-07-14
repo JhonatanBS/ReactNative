d3371e8b695cffc6db05c8817a9120727dfaee82
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2023-07-14 10:16:05 -0300

    Trying property SectionList

--- Classes/ignitegym/src/screens/History.tsx
+++ Classes/ignitegym/src/screens/History.tsx
@@ -1,14 +1,41 @@
-import { VStack } from "native-base";
+import { useState } from "react";
+
+import { Heading, VStack , SectionList} from "native-base";
 
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "26.08.22",
+      data: ["Remada unilateral", "Levantamento terra"]
+    },
+    {
+      title: "27.08.22",
+      data: ["Puxada frontal"]
+    },
+  ]);
+
   return (
     <VStack flex={1}>
-      <ScreenHeader title="Histórico de Exercícios"/>
+      <ScreenHeader title="Histórico de Exercícios" />
 
-      <HistoryCard />
+      <SectionList
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({section}) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
+
+
     </VStack>
   )
 }

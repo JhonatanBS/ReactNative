798ac47c5ae1bdb9c6bea994d39c106e1f9fc84a
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2023-08-23 15:43:46 -0300

    Started screen Exercise and using navigation

--- Classes/ignitegym/src/screens/Home.tsx
+++ Classes/ignitegym/src/screens/Home.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 import { VStack, FlatList, HStack, Heading, Text } from "native-base";
+
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
@@ -9,6 +12,12 @@ export function Home() {
   const [groups, setGroups] = useState(["Costas", "Tríceps", "Bíceps", "ombro"]);
   const [exercises, setExercises] = useState(["Puxada Frontal", "Remada curvada", "Remada unilateral", "Levantamento terra"]);
   const [groupSelected, setGroupSelected] = useState("Costas");
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate("exercise");
+  }
 
   return (
     <VStack flex={1}>
@@ -46,7 +55,9 @@ export function Home() {
           data={exercises}
           keyExtractor={item => item}
           renderItem={({ item }) => (
-            <ExerciseCard />
+            <ExerciseCard 
+              onPress={handleOpenExerciseDetails}
+            />
           )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{ paddingBottom: 20 }}

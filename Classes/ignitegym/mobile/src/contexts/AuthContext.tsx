9e0db3b59c98f6609f4900fe6aa2ef77d9e2e616
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2023-12-31 19:09:06 -0300

    Creating state for the user

--- Classes/ignitegym/mobile/src/contexts/AuthContext.tsx
+++ Classes/ignitegym/mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useState } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
 
@@ -13,16 +13,14 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-
+  const [ user, setUser ] = useState({
+    id: "1",
+    name: "Rodrigo Gonçalves",
+    email: "rodrigo@email.com",
+    avatar: "rodrigo.png"
+    });
   return(
-    <AuthContext.Provider value={{
-      user: {
-      id: "1",
-      name: "Rodrigo Gonçalves",
-      email: "rodrigo@email.com",
-      avatar: "rodrigo.png"
-      }
-    }}>
+    <AuthContext.Provider value={{ user }}>
     {children}
     </AuthContext.Provider>
   )

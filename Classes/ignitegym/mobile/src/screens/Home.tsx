f962ec2c0f5ecce146ba88b9893bc9f9c3091725
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-01-21 18:13:37 -0300

    Component Loading atived for wait the datas will loading

--- Classes/ignitegym/mobile/src/screens/Home.tsx
+++ Classes/ignitegym/mobile/src/screens/Home.tsx
@@ -11,8 +11,10 @@ import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
 import { ExerciseCard } from "@components/ExerciseCard";
 import { ExerciseDTO } from "@dtos/ExerciseDTO";
+import { Loading } from "@components/Loading";
 
 export function Home() {
+  const [ isLoading, setIsLoading ] = useState(true);
   const [groups, setGroups] = useState<string[]>([]);
   const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
   const [groupSelected, setGroupSelected] = useState("Costas");
@@ -43,6 +45,7 @@ export function Home() {
 
   async function fetchExercisesByGroup() {
     try {
+      setIsLoading(true);
       const response = await api.get(`/exercises/bygroup/${groupSelected}`);
       setExercises(response.data);
 
@@ -55,6 +58,8 @@ export function Home() {
         placement: "top",
         bgColor: "red.500"
       })
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -87,8 +92,11 @@ export function Home() {
         maxH={10}
         minH={10}
       />
-
-      <VStack flex={1} px={8}>
+      
+      { isLoading ? 
+        <Loading /> 
+      :
+        <VStack flex={1} px={8}>
         <HStack justifyContent="space-between" mb={5}>
           <Heading color="gray.200" fontSize="md" fontFamily="heading">
             Exercícios
@@ -111,7 +119,8 @@ export function Home() {
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{ paddingBottom: 20 }}
         />
-      </VStack>
+        </VStack>
+      }
     </VStack>
   )
 }

032dafcc0543c6826ddd35903515358bc2c614bb
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-01-21 17:53:10 -0300

    Loading images

--- Classes/ignitegym/mobile/src/components/ExerciseCard.tsx
+++ Classes/ignitegym/mobile/src/components/ExerciseCard.tsx
@@ -1,6 +1,8 @@
 import { HStack, Heading, Image, Text, VStack, Icon} from "native-base";
 import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 
+import { api } from "@services/api";
+
 import { Entypo } from "@expo/vector-icons";
 import { ExerciseDTO } from "@dtos/ExerciseDTO";
 
@@ -13,7 +15,7 @@ export function ExerciseCard({ data, ...rest }: Props) {
     <TouchableOpacity {...rest}>
       <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
         <Image
-          source={{ uri: "https://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg" }}
+          source={{ uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}` }}
           alt="Imagem do Exercício - Remada Unilateral"
           w={16}
           h={16}

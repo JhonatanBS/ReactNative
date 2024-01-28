658f856b68b10faef275e82dd544c86514708d8a
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-01-28 20:32:53 -0300

    Verifying if the token is invalid

--- Classes/ignitegym/mobile/src/services/api.ts
+++ Classes/ignitegym/mobile/src/services/api.ts
@@ -12,11 +12,17 @@ const api = axios.create({
 }) as APIInstanceProps;
 
 api.registerInterceptTokenManager = signOut => {
-  const interceptTokenManager = api.interceptors.response.use(response => response, error => {
-    if(error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message));
+  const interceptTokenManager = api.interceptors.response.use(response => response, requestError => {
+    if(requestError?.response?.status === 401) {
+      if(requestError.response.data?.message === "token.expired" || requestError.response.data?.message === "token.invalid") {
+        
+      }
+    }
+    
+    if(requestError.response && requestError.response.data) {
+      return Promise.reject(new AppError(requestError.response.data.message));
     } else {
-      return Promise.reject(error);
+      return Promise.reject(requestError);
     }
   });
 

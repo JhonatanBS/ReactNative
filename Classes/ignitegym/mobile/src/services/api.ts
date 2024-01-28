a16bc9dbc27e2d0a9a43518449573edfedebd70b
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-01-28 20:24:39 -0300

    Creating a manager of token

--- Classes/ignitegym/mobile/src/services/api.ts
+++ Classes/ignitegym/mobile/src/services/api.ts
@@ -1,16 +1,30 @@
 import { AppError } from "@utils/AppError";
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+
+type SignOut = () => void;
+
+type APIInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+}
 
 const api = axios.create({
   baseURL: "http://192.168.1.7:3333"
-});
+}) as APIInstanceProps;
 
-api.interceptors.response.use(response => response, error => {
-  if(error.response && error.response.data) {
-    return Promise.reject(new AppError(error.response.data.message));
-  } else {
-    return Promise.reject(error);
+api.registerInterceptTokenManager = signOut => {
+  const interceptTokenManager = api.interceptors.response.use(response => response, error => {
+    if(error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message));
+    } else {
+      return Promise.reject(error);
+    }
+  });
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
   }
-})
+}
+
+
 
 export { api };

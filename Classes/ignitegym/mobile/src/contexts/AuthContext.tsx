ce6614f94f4b1280a76d03f3bb5023bd8b5f8114
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-01-29 09:23:33 -0300

    Storing refresh token

--- Classes/ignitegym/mobile/src/contexts/AuthContext.tsx
+++ Classes/ignitegym/mobile/src/contexts/AuthContext.tsx
@@ -31,12 +31,12 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setUser(userData);
   }
 
-  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+  async function storageUserAndTokenSave(userData: UserDTO, token: string, refresh_token: string) {
     try {
       setIsLoadingUserStorageData(true);
 
       await storageUserSave(userData);
-      await storageAuthTokenSave(token);
+      await storageAuthTokenSave({token, refresh_token});
       
     } catch (error) {
       throw error;
@@ -49,9 +49,9 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       const { data } = await api.post("/sessions", { email, password });
 
-      if(data.user && data.token) {
+      if(data.user && data.token && data.refresh_token) {
         
-        await storageUserAndTokenSave(data.user, data.token);
+        await storageUserAndTokenSave(data.user, data.token, data.refresh_token);
         userAndTokenUpdate(data.user, data.token);
       }
 
@@ -90,7 +90,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     setIsLoadingUserStorageData(true);
 
     const userLogged = await storageUserGet();
-    const token = await storageAuthTokenGet();
+    const { token } = await storageAuthTokenGet();
 
     if( userLogged && token) {
       userAndTokenUpdate(userLogged, token);
--- Classes/ignitegym/mobile/src/services/api.ts
+++ Classes/ignitegym/mobile/src/services/api.ts
@@ -1,3 +1,4 @@
+import { storageAuthTokenGet } from "@storage/storageAuthToken";
 import { AppError } from "@utils/AppError";
 import axios, { AxiosInstance } from "axios";
 
@@ -12,10 +13,15 @@ const api = axios.create({
 }) as APIInstanceProps;
 
 api.registerInterceptTokenManager = signOut => {
-  const interceptTokenManager = api.interceptors.response.use(response => response, requestError => {
+  const interceptTokenManager = api.interceptors.response.use(response => response, async (requestError) => {
     if(requestError?.response?.status === 401) {
       if(requestError.response.data?.message === "token.expired" || requestError.response.data?.message === "token.invalid") {
-        
+        const { refresh_token } = await storageAuthTokenGet();
+
+        if(!refresh_token) {
+          signOut();
+          return Promise.reject(requestError);
+        }
       }
     }
     

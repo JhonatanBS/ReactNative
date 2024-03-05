d583138eee4b405c83ab08c7c632bac6c820bdcd
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-03-04 21:53:58 -0300

    Creating tag in the One Signal

--- Classes/igniteshoesapp/App.tsx
+++ Classes/igniteshoesapp/App.tsx
@@ -1,21 +1,26 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
-import OneSignal from 'react-native-onesignal';
+import { OneSignal } from 'react-native-onesignal';
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
 
+import { APP_ID } from "@env"
+
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
 
-OneSignal.setAppId(`${process.env.APP_ID}`);
+OneSignal.initialize(APP_ID);
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
+  tagUserEmailCreate("jhonatanb.s4nt0s@gmail.com");
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
---
+++ Classes/igniteshoesapp/src/notifications/notificationsTags.ts
@@ -0,0 +1,6 @@
+import { OneSignal } from "react-native-onesignal";
+
+export function tagUserEmailCreate(email: string) {
+  OneSignal.User.addTag("user_email", email);
+}
+

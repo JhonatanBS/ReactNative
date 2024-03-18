dbefd6dc60e4ee3477a4dfc07befe4d47607e1a6
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-03-17 23:47:38 -0300

    Adding project ignite quiz

--- Classes/igniteshoesapp/src/notifications/notificationsTags.ts
+++ Classes/igniteshoesapp/src/notifications/notificationsTags.ts
@@ -1,6 +1,6 @@
 import { OneSignal } from "react-native-onesignal";
 
 export function tagUserEmailCreate(email: string) {
-  OneSignal.User.addTag("user_email", email);
+  OneSignal.User.removeTag("user_email");
 }
 

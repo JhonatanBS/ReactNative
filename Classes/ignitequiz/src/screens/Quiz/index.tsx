b0be4ba57e702da92ea97dfb9485767b1c77184c
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-03-26 10:14:13 -0300

    Animate of discard

--- Classes/ignitequiz/src/screens/Quiz/index.tsx
+++ Classes/ignitequiz/src/screens/Quiz/index.tsx
@@ -36,6 +36,8 @@ interface Params {
 
 type QuizProps = typeof QUIZ[0];
 
+const CARD_INCLINATION = 10;
+
 export function Quiz() {
   const [points, setPoints] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
@@ -158,10 +160,28 @@ export function Quiz() {
   const onPan = Gesture
   .Pan()
   .onUpdate((event) => { 
-    cardPosition.value = event.translationX;
+    const moveToLeft = event.translationX < 0;
+
+    if(moveToLeft) {
+      cardPosition.value = event.translationX;
+    }
   })
   .onEnd(() => {
     cardPosition.value = withTiming(0);
+  });
+
+  const dragStyles = useAnimatedStyle(() => {
+    const rotateZ = cardPosition.value / CARD_INCLINATION;
+    return {
+      transform: [
+        { 
+          translateX: cardPosition.value
+        },
+        {
+          rotateZ: `${rotateZ}deg`
+        }
+      ]
+    }
   });
 
   useEffect(() => {
@@ -210,7 +230,7 @@ export function Quiz() {
 
         <GestureDetector gesture={onPan}>
 
-        <Animated.View style={shakeStyleAnimated}>
+        <Animated.View style={[shakeStyleAnimated, dragStyles]}>
           <Question
             key={quiz.questions[currentQuestion].title}
             question={quiz.questions[currentQuestion]}
